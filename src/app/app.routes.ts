db4ba6548399ab947a105a54580b76e651599d36
FelipeAkel <felipe.akel01@gmail.com>
2024-08-14 04:16:49 -0300

    Primeiro component e seu HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaComponent } from './modulos/lista-de-tarefas/page/lista/lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListaComponent,
+  }
+];

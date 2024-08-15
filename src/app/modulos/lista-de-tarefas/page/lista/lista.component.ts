739978a522142805d9501abc1ed980ba63e677b2
FelipeAkel <felipe.akel01@gmail.com>
2024-08-14 22:22:56 -0300

    Tratando os dados do input

--- src/app/modulos/lista-de-tarefas/page/lista/lista.component.ts
+++ src/app/modulos/lista-de-tarefas/page/lista/lista.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss'
 })

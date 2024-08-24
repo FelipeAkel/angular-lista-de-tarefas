416c3a62061dc078781ba9272e6e4fcab1cc7d3b
FelipeAkel <felipe.akel01@gmail.com>
2024-08-24 10:15:55 -0300

    HTML da Lista de Tarefas

--- src/app/modulos/lista-de-tarefas/components/input-lista-itens/input-lista-itens.component.ts
+++ src/app/modulos/lista-de-tarefas/components/input-lista-itens/input-lista-itens.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Interface
+import { IListaItens } from '../../interface/IListaItens.interface';
 
 @Component({
   selector: 'app-input-lista-itens',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input-lista-itens.component.html',
   styleUrl: './input-lista-itens.component.scss'
 })
 export class InputListaItensComponent {
+
+
+  @Input({ required:true }) public inputListaItens: IListaItens[] = [];
 
 }
